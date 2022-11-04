febc44ba3473bf0ef862349e98d80fa113e49647
blakeleyc <116079051+blakeleyc@users.noreply.github.com>
2022-11-03 21:38:08 -0400

    Alien Invasion Lab and Homework

---
+++ bluesky.py
@@ -0,0 +1,10 @@
+import pygame
+import time
+pygame.init()
+# set the screen size
+screen = pygame.display.set_mode((200,200))
+# set this to blue
+screen.fill((0,0,230))
+
+pygame.display.flip()
+time.sleep(10)
--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 20
         # Screen Settings
         self.ship_speed = 1.5
         self.screen_width = 1200
